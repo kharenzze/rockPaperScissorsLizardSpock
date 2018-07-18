d50d3d26c1b36fb1585112ec85eed9c81ee7f5f4
kharenzze <str237@gmail.com>
2018-07-19 00:09:03 +0200

    Added server/client mode detection

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#define SERVER_PORT 9000
+#define SERVER_MODE "server"
+
+using std::string;
+
+void logPort(uint32_t port) {
+    std::cout << "Running on port " << port << std::endl;
+}
+
+void server() {
+    std::cout << "Welcome to RockPaperScissorsLizardSpock server" << std::endl;
+    uint32_t port = SERVER_PORT;
+    logPort(port);
+}
+
+void client() {
+
+}
+
+int main(int argc, char* argv[]) {
+    bool isServer = false;
+    if (argc == 2) {
+        const auto mode = string(argv[1]);
+        isServer = mode == SERVER_MODE;
+    }
+
+    if (isServer) {
+        server();
+    } else {
+        client();
+    }
+
     return 0;
 }
